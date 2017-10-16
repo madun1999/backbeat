5d3611aafd369c2ae3f8008652c26c51d2860c09
Zachary Oldham <zacharyoldham@gmail.com>
2017-10-16 13:20:21 -0500

    More documentation updates

--- backend.py
+++ backend.py
@@ -1,11 +1,11 @@
 # This file contains the backend methods that will process the output of Essentia and provide understandable feedback based on this.
 
-# Method to determine where the player slowed/sped up and return a string describing this.
-# Accepts an array of integers each representing the tempo in a given measure and an integer representing the target tempo.
-# Also accepts an integer representing the allowable margin of error. 
-# temopArr is an array of integers each representing the average tempo of one measure.
-# targetTempo is the tempo the player was aiming to play at.
-# marginOfError represents the allowable divergence from the target without a comment.
+# Method to determine where the player slowed/sped up and return a string describing this
+# Accepts an array of integers each representing the tempo in a given measure and an integer representing the target tempo
+# Also accepts an integer representing the allowable margin of error
+# temopArr is an array of integers each representing the average tempo of one measure
+# targetTempo is the tempo the player was aiming to play at
+# marginOfError represents the allowable divergence from the target without a comment
 # Returns a string describing the patterns of the musician
 def getAnalysis(tempoArr, targetTempo, marginOfError):
     # Create a list of lists. Each sublist will contain a string denoting 'fast' 'slow' or 'steady', 
