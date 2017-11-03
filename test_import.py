7ad82a149a4c0ea3a107366510deee4aa8539fcb
gajankumar1016 <gajankumar1016@gmail.com>
2017-11-03 17:26:11 -0500

    Removed some unnecesary files

--- test_import.py
+++ test_import.py
@@ -2,7 +2,7 @@ from analyze_beats import*
 from backend import*
 
 fname = 'sample_wav_files/air_force_song.wav'
-#print(getMeasures('sample_wav_files/air_force_song.wav', 4, 0.25))
-
-print(get_bpm_for_constant_fractions_of_wav_file(fname, 0.0, 6))
+#print(getMeasures('sample_wav_files/africa-toto.wav', 4, 0.95))
+print(getMeasures('sample_wav_files/air_force_song.wav', 4, 0.95))
+#print(get_bpm_for_constant_fractions_of_wav_file(fname, 0.0, 6))
 
