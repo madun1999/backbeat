2235f3b65bcf256f1972d9db4cdb1b04691e5b90
gajankumar1016 <gajankumar1016@gmail.com>
2017-11-10 16:04:05 -0600

    Loading audio once worked with getMeasures()! Using a 3.42 min piece of music, the running time was cut from 18.2 minutes to 3.7 minutes. Now need to figure out how to copy the revised getMeasures() in analyze_beats to backend.py without getting a merge conflict.

--- test_import.py
+++ test_import.py
@@ -1,15 +1,28 @@
-from analyze_beats import*
-from backend import*
+import analyze_beats
+import backend
+from timeit import Timer
 
 fname = 'sample_wav_files/air_force_song.wav'
+#fname = 'sample_wav_files/africa-toto.wav'
+
+print('getMeasures loading audio once: ')
+t = Timer("""analyze_beats.getMeasures('sample_wav_files/mambo.wav', 4, 0.95)""", setup="""import analyze_beats""")
+print t.timeit(1)
+
+print('getMeasures loading audio every time: ')
+t = Timer("""backend.getMeasures('sample_wav_files/mambo.wav', 4, 0.95)""", setup="""import backend""")
+print t.timeit(1)
+
+
+
 #print(getMeasures('sample_wav_files/africa-toto.wav', 4, 0.95))
 #print(getMeasures('sample_wav_files/air_force_song.wav', 4, 0.95))
-print(get_bpm_for_constant_fractions_of_wav_file(fname, 0.0, 4))
+#print(get_bpm_for_constant_fractions_of_wav_file(fname, 0.0, 4))
 
-audio = get_audio_from_wav_path(fname)
-length = get_length_of_wav_file_seconds(fname)
+#audio = get_audio_from_wav_path(fname)
+#length = get_length_of_wav_file_seconds(fname)
 
-for i in range(0, 4):
-    start = i*10
-    end = i*10 + 10
-    print(get_bpm_from_audio_array(audio, 0.0, start, end, length))
+#for i in range(0, 4):
+#    start = i*10
+#    end = i*10 + 10
+#    print(get_bpm_from_audio_array(audio, 0.0, start, end, length))
