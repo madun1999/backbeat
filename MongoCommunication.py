fea55759f630226511cb9cfa96281134c3b20663
Kavi Ravuri <kaviravuri888@gmail.com>
2017-11-10 16:28:30 -0600

    Add files via upload

--- MongoCommunication.py
+++ MongoCommunication.py
@@ -8,29 +8,37 @@ def homepage():
     data = request.data
     dataDict = json.loads(data)
     client = MongoClient('mongodb://kravuri:<backbeat2>@cluster0-shard-00-00-5jiht.mongodb.net:27017,cluster0-shard-00-01-5jiht.mongodb.net:27017,cluster0-shard-00-02-5jiht.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin')
+    fcram = client['xcram']['users'].find_one({"name":dataU, "tempsRates":dataP})
+    client['xcram']['users'].insert_one(
+                                        {"name":dataDict["name"],
+                                        "tempRates":dataDict["tempRates"],
+                                        "topic":[]}
+                                        )
 
 @app.route('/topic', methods = ['GET'])
 def bpm():
-    data = request.args.get("bpm")
+    data = request.args.get("tempRates")
     client = MongoClient('mongodb://kravuri:<backbeat2>@cluster0-shard-00-00-5jiht.mongodb.net:27017,cluster0-shard-00-01-5jiht.mongodb.net:27017,cluster0-shard-00-02-5jiht.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin')
-    return null
+    fcram = client['xcram']['users'].find_one({"name":data})
+    if(fcram):
+        return dumps(fcram)
+    return "Invalid Song name"
 
-    
+
 @app.route('/topic/add', methods = ['PUT'])
 def slowOrFast():
         datav = request.data
         dataDict = json.loads(datav)
-        data = dataDict.get("")
+        data = dataDict.get("table")
         client = MongoClient('mongodb://kravuri:<backbeat2>@cluster0-shard-00-00-5jiht.mongodb.net:27017,cluster0-shard-00-01-5jiht.mongodb.net:27017,cluster0-shard-00-02-5jiht.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin')
-        return null
-    
+
+
 @app.route('/topic/delete', methods = ['PUT'])
 def measureNumbers():
         datav = request.data
         dataDict = json.loads(datav)
         data = dataDict.get("")
         client = MongoClient('mongodb://kravuri:<backbeat2>@cluster0-shard-00-00-5jiht.mongodb.net:27017,cluster0-shard-00-01-5jiht.mongodb.net:27017,cluster0-shard-00-02-5jiht.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin')
-        return null
-    
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
